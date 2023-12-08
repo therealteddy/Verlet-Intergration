189d6f98830796c0597b08b61f46708faab47ac1
Ted Jerin <80974655+therealteddy@users.noreply.github.com>
2023-12-08 12:49:26 +0200

    Added a lot of stuff.

--- include/angularmotion.h
+++ include/angularmotion.h
@@ -5,9 +5,15 @@
 #include "verlet.h"
 #include <math.h>
 
+enum Shapes {PLATE, DISK, RING}; 
+
 typedef struct {
     double AngularVelocity; 
-    double Rotation;     
+    double AngularAcceleration; 
+    double AngularDisplacement;
+    double Rotation;  
+    double Inertia;   
+    double Mass; 
 } AngularObject; 
 
 /* Convert Degrees to radians */
@@ -17,6 +23,27 @@ double ToDegrees(double Radians);
 double ToRadians(double Degrees); 
 
 /* Calculate rotation from gravity as torque */ 
-void GetAngularRotation(AngularObject* Object, Vector LinearVelocity, double radius, bool colliding); 
+void AngularDisplacementFromLinearVelocity(AngularObject* Object, Vector LinearVelocity, double radius, bool colliding, unsigned int Shape); 
+
+/* Calculate moment of inertia of a plate */
+double Plate_GetMomentOfInertia(double mass, double a, double b);
+
+/* Calculate moment of inertia of a disk */
+double Disk_GetMomentOfInertia(double mass, double radius); 
+
+/* Calculate moment of inertia of a ring */
+double Ring_GetMomentOfInertia(double mass, double radius);
+
+/* Calculate angular momentum */ 
+double GetAngularMomentum(AngularObject* Object); 
+
+/* Get torque of a plate assuming rotational axis is at the center */  
+double Plate_GetTorque(double force, double height, double width); 
+
+/* Get angular acceleration from torque and inertia */
+void GetAngularAcceleration(AngularObject* Object, double torque); 
+
+/* Get Angular Displacement from angular velocity */
+void GetAngularDisplacement(AngularObject* Object); 
 
 #endif /* ANGULAR_MOTION_H */
--- src/angularmotion.c
+++ src/angularmotion.c
@@ -9,11 +9,66 @@ double ToRadians(double Degrees) {
     return (double) (Degrees/360.0f) * (2*Pi);
 }
 
-/* There are so many things wrong with this function Ted. Fix later! */
-void GetAngularRotation(AngularObject* Object, Vector LinearVelocity, double radius, bool colliding) {
-    double LinearVelocityMagnitude = VectorMagnitude(LinearVelocity); 
-    if (!(colliding)){ 
-        Object->AngularVelocity += LinearVelocityMagnitude * dt() * radius; 
-        Object->Rotation = ToRadians(Object->AngularVelocity);
-    }  else Object->Rotation = Object->AngularVelocity = 0.0f;
+double Plate_GetMomentOfInertia(double mass, double a, double b) {
+    return (mass/12) * ((a*a) + (b*b)); 
+}
+
+double Disk_GetMomentOfInertia(double mass, double radius) {
+    return (mass/2) * (radius*radius); 
+}
+
+double Ring_GetMomentOfInertia(double mass, double radius) {
+    return (mass) * (radius*radius);
+}
+
+double GetAngularMomentum(AngularObject* Object) {
+    return (Object->AngularVelocity * Object->Inertia); 
+}
+
+double Plate_GetTorque(double force, double width, double height) {
+    double hw = height/2; 
+    double ww = width/2; 
+    double r = sqrt((hw)*(hw) + (ww)*(ww)); 
+    return (r * force) + (-r * force); /* Assuming origin is center (width/2, height/2)*/
+}
+
+void GetAngularAcceleration(AngularObject* Object, double torque) {
+    Object->AngularAcceleration = torque/Object->Inertia; 
+}
+
+void GetAngularVelocity(AngularObject* Object) {
+    Object->AngularVelocity += Object->AngularAcceleration * dt(); 
+}
+
+void GetAngularDisplacement(AngularObject* Object) {
+    Object->AngularDisplacement += Object->AngularVelocity * dt();  
+}
+
+void AngularDisplacementFromLinearVelocity(AngularObject* Object, Vector LinearVelocity, double radius, bool colliding, unsigned int Shape) {
+    /* Get Resultant Vector of Linear Velocity */ 
+    double LinearResulatant = VectorMagnitude(LinearVelocity); 
+
+    /* Get Moment of inertia assuming uniform mass */ 
+    switch (Shape)
+    {
+    case DISK:
+        Object->Inertia = Disk_GetMomentOfInertia(Object->Mass, radius); 
+        break;
+    
+    case RING: 
+        Object->Inertia = Ring_GetMomentOfInertia(Object->Mass, radius);
+        break; 
+
+    case PLATE: 
+        //Implement later; 
+        break; 
+
+    default:
+        break;
+    }
+
+    if (!colliding) {
+        Object->AngularVelocity += (LinearResulatant/radius) * dt(); 
+        Object->AngularDisplacement += Object->AngularVelocity; 
+    } else Object->AngularVelocity = 0.0f; 
 }
